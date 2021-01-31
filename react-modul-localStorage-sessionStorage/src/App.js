2796453b403891f6c7411a80b8b6d30fccb04883
lathifindarosarimahida <lathifinda_rosari_27rpl@student.smktelkom-mlg.sch.id>
2021-01-31 23:06:23 +0700

    Menambahkan file

---
+++ react-modul-localStorage-sessionStorage/src/App.js
@@ -0,0 +1,30 @@
+import React from "react"
+import Navbar from './components/Navbar'
+import {Switch, Route} from "react-router-dom"
+
+// import semua halaman yang ditampilkan
+import Home from "./pages/Home"
+import About from './pages/About'
+// import Contact from "./pages/Contact"
+import Gallery from "./pages/Gallery"
+import Cart from "./pages/Cart"
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          {/* <Route path="/contact" component={Contact} /> */}
+          <Route path="/gallery" component={Gallery} />
+          <Route path="/cart" component={Cart} />
+        </Switch>
+
+      </div>
+    )
+  }
+}
+
+export default App;
